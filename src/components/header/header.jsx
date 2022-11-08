1d1b5785a3645c7895a85ce0da0daab4f519c179
Robert Ball <bobball89@gmail.com>
2022-11-08 01:58:57 +0000

    updated header style with mui Appbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,18 @@
+import { AppBar, Toolbar, IconButton, Typography} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 function Header() {
   return (
-    <>
-      <h1>our store</h1>
-    </>
+<AppBar position="static">
+  <Toolbar variant="dense">
+    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" color="inherit" component="div">
+      Our Store
+    </Typography>
+  </Toolbar>
+</AppBar>
   );
 }
 
